f7574aa87dda7a0c1e716c6ef7d8e40e5e3b613c
ShaoWei Huang <aaron0327@gmail.com>
2024-11-22 19:18:12 +0800

    Add standard deviation method to utils package
    Add standard deviation method. This is used to caculate
    bollinger bands.

--- utils/utils.go
+++ utils/utils.go
@@ -19,3 +19,16 @@ func FindMinMax(data []data.TradingData, start int, end int) (float32, float32) 
 	}
 	return min, max
 }
+
+func StandardDeviation(data []data.TradingData, sma []float32, n int) []float32 {
+	var stdDev []float32
+	var sum = float32(0)
+	for i := 0; i < len(data)-n; i++ {
+		for j := i; j < i+n; j++ {
+			diff := data[j].Close - sma[i]
+			sum += diff * diff
+		}
+		stdDev = append(stdDev, float32(math.Sqrt(float64(sum)/float64(n))))
+	}
+	return stdDev
+}
